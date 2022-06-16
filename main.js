6c5b3999e012f3cd5996a90c4150af4bc1157f37
yehuda-sanay <yehudasanay1@gmail.com>
2022-06-16 18:14:29 +0300

    16.06.22

---
+++ main.js
@@ -0,0 +1,13 @@
+const user_info={
+    age:18,
+    firstName:"yehuda",
+    lastName:"sanay",
+}
+function printUserInfo(user_info){
+    user_info.forEach(element => {
+        if(user_info.age>1){
+        h1_id.innerHTML=element[1];}
+    });
+
+}
+printUserInfo()
