ceaba5615e51742ee76bc36eaaffdffeb9b0fc48
yehuda-sanay <yehudasanay1@gmail.com>
2022-07-14 14:33:45 +0300

    14.07.22

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const user_info={
-    age:18,
-    firstName:"yehuda",
-    lastName:"sanay",
-};
-function printUserInfo(){
-    for (let key in user_info) {
-        h1_id.innerHTML+=`${key} : ${user_info[key]}<br/>`
-    }
+// const user_info={
+//     age:18,
+//     firstName:"yehuda",
+//     lastName:"sanay",
+// };
+// function printUserInfo(){
+//     for (let key in user_info) {
+//         h1_id.innerHTML+=`${key} : ${user_info[key]}<br/>`
+//     }
 
-}
-printUserInfo()
+// }
+// printUserInfo()
+
+// let myArray=document.getElementsByClassName("sehela1");
+// for (const i of myArray) {
+//     console.log(i);
+// }
+
+// console.log(myArray);
+
